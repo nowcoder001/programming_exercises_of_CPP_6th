2740fe7c6680cf15d09feaf37617ba028d544a92
niutianhua <niutianhua@163.com>
2019-10-09 11:36:26 +0800

    committed

--- chapter_6/6.16.5.c
+++ chapter_6/6.16.5.c
@@ -1,10 +1,10 @@
 /*Have a program request the user to enter an uppercase letter. Use nested loops to produce
 a pyramid pattern like this:
-A
-ABA
-ABCBA
-ABCDCBA
-ABCDEDCBA
+        A
+       ABA
+      ABCBA
+     ABCDCBA
+    ABCDEDCBA
 The pattern should extend to the character entered. For example, the preceding pattern
 would result from an input value of E. Hint: Use an outer loop to handle the rows. Use
 three inner loops in a row, one to handle the spaces, one for printing letters in ascending
@@ -15,8 +15,37 @@ programming exercise 3.*/
 #include<stdio.h>
 int main(void)
 {
-    char ch;
+    int ROWS;
+    int space;
+    int row;
+    int i;
+    int j;
+    char ch_f;
+    char ch_b;
+    char input;
+
     printf("Enter a uppercase letter: ");
-    scanf("%c",ch)
+    scanf("%c",&input);
+    ROWS = input - 64;
+    for(row = 0; row < ROWS; row++)  //Handle the rows
+    {
+        for(space = 0; space < 5 - row; space++)  //空格
+        {
+            printf(" ");
+        }
+        for(ch_f = 'A', i = 0; i <= row; i++)   //升序
+        {
+            printf("%c",ch_f++);
+        }
+        for(i = 0,ch_b = row + 64; i <= row -1; i++)   //降序
+        {
+            printf("%c",ch_b--);
+        }
+        for(space = 0; space < 4 - row; space++)  //空格
+        {
+            printf(" ");
+        }
+        printf("\n");
+    }
     return 0;
 }
