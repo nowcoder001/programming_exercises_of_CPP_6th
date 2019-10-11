d2b3b9ed40c299241a526e97a88e211e9542c5a3
niutianhua <niutianhua@163.com>
2019-10-11 11:42:01 +0800

    committed

--- chapter_6/6.16.7.c
+++ chapter_6/6.16.7.c
@@ -7,6 +7,16 @@ the array.*/
 
 int main(void)
 {
-    
+    char word[20];
+    int index;
+    int i;
+    printf("Enter a single word: ");
+    scanf("%s",word);
+    index = strlen(word);
+    for(i = 0; i <= index;index--)
+    {
+        printf("%c",word[index]);
+    }
+    printf("\n");
     return 0;
 }
