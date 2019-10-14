31fd1378dffc99b557104c1dbe4cc087beaae073
niutianhua <niutianhua@163.com>
2019-10-14 17:31:46 +0800

    committed

--- chapter_6/6.16.14.c
+++ chapter_6/6.16.14.c
@@ -13,5 +13,30 @@ second array displayed below the corresponding element of the first array.*/
 #include<stdio.h>
 int main(void)
 {
-    
+    int i,j;
+    int count = 7;
+    double sum = 0;
+    double sum1;
+    double array1[8];
+    double array2[8];
+    printf("Enter values for the eight-elements of the first array: ");
+    for(i = 0; i <= count; i++)
+    {
+       scanf("%lf",&array1[i]);   
+    }
+    for(i = 1, array2[0] = array1[0]; i <= count; i++)
+    {
+        array2[i] = array2[i-1] + array1[i];  
+    }  
+    for(i = 0; i <= count; i++) 
+    {
+        printf("%-10f ",array1[i]);
+    }
+    printf("\n");
+    for(j = 0; j <= count;j++)
+    {
+        printf("%-10f ",array2[j]);
+    }
+    printf("\n");
+    return 0;
 }
---
+++ chapter_6/6.16.15.c
@@ -0,0 +1,15 @@
+/*Write a program that reads in a line of input and then prints the line in reverse order. 
+You can store the input in an array of char; assume that the line is no longer than 255 
+characters. Recall that you can use scanf() with the %c specifier to read a character at 
+a time from input and that the newline character ( \n) is generated when you press the 
+Enter key.*/
+
+#include<stdio.h>
+int main(void)
+{
+    int i;
+    int index;
+    char ch[254];
+    printf("Enter a lime of character: ");
+    for()
+}
