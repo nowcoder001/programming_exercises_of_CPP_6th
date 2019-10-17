a4c15286111d080d6ce18cc2cf12f0a398c9c9f0
niutianhua <niutianhua@163.com>
2019-10-17 17:21:33 +0800

    committed

--- test.c
+++ test.c
@@ -1,16 +1,17 @@
-#include <stdio.h>
-#define	INITIAL_NUMBER 5   //初始朋友数为5人
-#define DUNBAR_NUMBER 150  //邓巴数
+#include<stdio.h>
+#include<ctype.h>
+#include<stdbool.h>
+#define STOP '|'
+
 int main(void)
 {
-    int week;
-    int number = INITIAL_NUMBER;
+    char c;
+    char prev;
+    long n_chars = 0L;
+    int n_lines = 0;
+    int n_words = 0;
+    int p_lines = 0;
+    bool inword = false;
 
-    for (week = 1; number <= DUNBAR_NUMBER; week++)
-    {
-        number = (number - week) * 2;		//计算每周的朋友数量
-        printf("After %d week, the number of Rabnud's friends is %d\n", week, number);
-    }
-
-    return 0;
+    
 }
