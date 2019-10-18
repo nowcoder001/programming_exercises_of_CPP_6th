f47bafb1b37a4db98cfefdaabe1629efe895d19b
niutianhua <niutianhua@163.com>
2019-10-19 01:56:12 +0800

    committed

---
+++ chapter_7/7.12.3.c
@@ -0,0 +1,37 @@
+/*Write a program that reads integers until 0 is entered. After input terminates, the
+program should report the total number of even integers (excluding the 0) entered, the
+average value of the even integers, the total number of odd integers entered, and the
+average value of the odd integers.*/
+
+#include<stdio.h>
+int main(void)
+{
+    int num;
+    int even_count = 0;
+    int odd_count = 0;
+    int sum_even = 0;
+    int sum_odd = 0;
+    printf("Enter integers, each number separate by space(terminate with 0): \n");
+    while(scanf("%d",&num))
+    {   
+        if(num == 0)
+        {
+            even_count++;
+            break;
+        }
+        else if(num%2 == 0)
+        {
+            even_count++;
+            sum_even += num;
+        } 
+        else 
+        {
+            odd_count++;
+            sum_odd += num;            
+        }
+    }
+    printf("The total number of even integers entered is %d, the average value of even integers is %.2f\n",even_count,(double)sum_even/even_count);
+    printf("The total number of odd integers entered is %d, the average value of odd integers is %.2f\n",odd_count,(double)sum_odd/odd_count);
+
+    return 0;
+}
---
+++ chapter_7/7.12.4.c
@@ -0,0 +1,9 @@
+/*Using if else statements, write a program that reads input up to #, replaces each period
+with an exclamation mark, replaces each exclamation mark initially present with two
+exclamation marks, and reports at the end the number of substitutions it has made.*/
+
+#include<stdio.h>
+int main(void)
+{
+    
+}
