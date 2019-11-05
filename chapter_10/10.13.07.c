9d2d37a1496bfc87155479d915979074472b170f
niutianhua <niutianhua@163.com>
2019-11-05 11:35:10 +0800

    committed chapter_10

--- chapter_10/10.13.07.c
+++ chapter_10/10.13.07.c
@@ -4,9 +4,28 @@ two-dimensional array is an array of arrays, a one-dimensional copy function can
 with each subarray.)*/
 
 #include<stdio.h>
+#define ELEMENT 2
+#define SUBARRAY 3
+void copy_ptr(double *target, double *source, int size);
 
 int main(void)
 {
-    
+    int i, j;
+    int count;
+    double ar[ELEMENT][SUBARRAY];
+    double array[ELEMENT][SUBARRAY] = {{1, 2, 3}, {4, 5, 6}};
+
+    for(count = 0; count < ELEMENT; count++)
+        copy_ptr(ar[count], array[count],SUBARRAY);
+    for(i = 0; i < ELEMENT; i++)
+        for(j = 0; j < SUBARRAY; j++)
+            printf("%f ",ar[i][j]);
+
     return 0;
+}
+void copy_ptr(double *target, double *source, int size)
+{
+    int i;
+    for(i = 0; i < size; i++)
+        *(target + i) = *(source + i);
 }
---
+++ chapter_10/10.13.08.c
@@ -0,0 +1,13 @@
+/*Use a copy function from Programming Exercise 2 to copy the third through fifth 
+elements of a seven-element array into a three-element array. The function itself need
+not be altered; just choose the right actual arguments. (The actual arguments need not be 
+an array name and array size. They only have to be the address of an array element and a 
+number of elements to be processed.)*/
+
+#include<stdio.h>
+
+int main(void)
+{
+    
+    return 0;
+}
