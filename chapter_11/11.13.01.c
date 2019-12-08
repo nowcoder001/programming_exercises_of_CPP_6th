fedff03a6077f6ca6b5b140f5bbf9958b09a1f59
niutianhua <niutianhua@163.com>
2019-12-08 14:41:09 +0800

    committed

---
+++ chapter_11/11.13.01.c
@@ -0,0 +1,10 @@
+/*Design and test a function that fetches the next n characters from input (including
+blanks, tabs, and newlines), storing the results in an array whose address is passed as an
+argument.*/
+
+#include<stdio.h>
+int main(void)
+{
+    
+    return 0;
+}
